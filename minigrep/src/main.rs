b688c7df16e95cf9a353b12d79722f5fdb4c533c
Jordan Rhodes <jsrhodes15@gmail.com>
2021-11-24 11:54:16 -0500

    Improve error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,34 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    // Because run returns () in the success case, we only care about detecting an error, so we don’t need unwrap_or_else to return the unwrapped value because it would only be ().
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e)
+    }
+}
+
+
+// Use trait object. Function will return a type that implements the Error trait
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    // Indicate that we are only using 'run' for side effects, not returning anything (unless there is an error)
+    Ok(())
 }
 
 struct Config {
@@ -21,10 +37,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
