ab56767a1b996f0f81df104a134a6d8cc69f0dd7
Jordan Rhodes <jsrhodes15@gmail.com>
2021-11-26 15:36:08 -0500

    Grep file, case-sensitivity based on env variable

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,9 +11,6 @@ fn main() {
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     // Because run returns () in the success case, we only care about detecting an error, so we don’t need unwrap_or_else to return the unwrapped value because it would only be ().
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e)
