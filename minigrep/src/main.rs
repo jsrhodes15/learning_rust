5511f467b3a7ed6984f6a3e11a90b8619b0cfe42
Jordan Rhodes <jsrhodes15@gmail.com>
2021-11-26 15:39:18 -0500

    Print errors to standard error stream

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
     // Because run returns () in the success case, we only care about detecting an error, so we don’t need unwrap_or_else to return the unwrapped value because it would only be ().
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e)
+        eprintln!("Application error: {}", e);
+        process::exit(1)
     }
 }
